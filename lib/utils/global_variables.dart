8f3bd0b8ac7451495571a166cbc098e14ff3e363
Ninad Belorkar <ninad.belorkar23@vit.edu>
2025-01-17 20:34:29 +0530

    Added recommendation code in search screen

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_clone_flutter/screens/add_post_screen.dart';
-import 'package:instagram_clone_flutter/screens/feed_screen.dart';
-import 'package:instagram_clone_flutter/screens/profile_screen.dart';
-import 'package:instagram_clone_flutter/screens/search_screen.dart';
+import 'package:mlsc_uninstagram_project/screens/add_post_screen.dart';
+import 'package:mlsc_uninstagram_project/screens/feed_screen.dart';
+import 'package:mlsc_uninstagram_project/screens/profile_screen.dart';
+import 'package:mlsc_uninstagram_project/screens/search_screen.dart';
 
 const webScreenSize = 600;
 
